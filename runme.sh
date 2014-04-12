7cd8e460874fbbc04cb450f1be15670b36d1d99c
Fredrik Gustafsson <gusten@dindinoh.net>
2014-04-12 09:02:45 +0200

    added structure

--- runme.sh
+++ runme.sh
@@ -2,14 +2,14 @@
 
 if [ ! -z $2 ]
 then
-    echo sudo apt-get -y install git
-    echo cd ~
+    sudo apt-get -y install git
     git clone https://github.com/dindinoh/coobradebianmailserver.git
     sed -i -e "s/coobrauser/$2/g" coobradebianmailserver/recipes/default.rb
     sed -i -e "s/coobradomain/$1/g" coobradebianmailserver/recipes/default.rb
+    mv coobradebianmailserver cookbooks/
     echo "cookbook_path '$(pwd)/cookbooks'" > solo.rb
     curl -L https://www.opscode.com/chef/install.sh | sudo bash
-    chef-solo -c ./solo.rb coobradebianmailserver::default
+    sudo chef-solo -c ./solo.rb coobradebianmailserver::default
 else
     echo "example: runme.sh maildomainname youruseraccountontheserver"
 fi
