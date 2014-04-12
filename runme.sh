e191f9385919063c247c5732e755467ccb78fea7
Fredrik Gustafsson <gusten@dindinoh.net>
2014-04-12 09:46:24 +0200

    runme.sh fix

--- runme.sh
+++ runme.sh
@@ -6,6 +6,7 @@ then
     git clone https://github.com/dindinoh/coobradebianmailserver.git
     sed -i -e "s/coobrauser/$2/g" coobradebianmailserver/recipes/default.rb
     sed -i -e "s/coobradomain/$1/g" coobradebianmailserver/recipes/default.rb
+    mkdir cookbooks
     mv coobradebianmailserver cookbooks/
     echo "cookbook_path '$(pwd)/cookbooks'" > solo.rb
     curl -L https://www.opscode.com/chef/install.sh | sudo bash
